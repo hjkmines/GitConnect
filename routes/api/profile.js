688aad6f082c3916128ebb5d250b3715414cbb2f
Hyung Kim <hjkmines@gmail.com>
2020-08-19 13:06:04 -0600

    create user profile if not found

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -53,6 +53,46 @@ async (req, res) => {
         medium, 
         twitter 
     } = req.body; 
+
+    //builds the profile object 
+    const profileFields = {}; 
+    profileFields.user = req.user.id; 
+    if (company) profileFields.company = company; 
+    if (website) profileFields.website = website; 
+    if (location) profileFields.location = location; 
+    if (bio) profileFields.bio = bio; 
+    if (status) profileFields.status = status; 
+    if (githubusername) profileFields.githubusername = githubusername; 
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim()); 
+    }
+
+    //Build social media object 
+    profileFields.social = {}; 
+    if (linkedin) profileFields.social.linkedin = linkedin; 
+    if (youtube) profileFields.social.youtube = youtube; 
+    if (medium) profileFields.social.medium = medium; 
+    if (twitter) profileFields.social.twitter = twitter; 
+
+    try {
+        let profile = await Profile.findOne({user: req.user.id}); 
+
+        if(profile) {
+            //updates the profile 
+            profile = await Profile.findOneAndUpdate({user: req.user.id}, {$set: profileFields}, {new: true}); 
+
+            return res.json(profile); 
+        }
+
+        //Create a new profile 
+        profile = new Profile(profileFields); 
+        
+        await Profile.save(); 
+        res.json(profile); 
+    } catch(err) {
+        console.error(err.message); 
+        res.status(500).send('Server Error'); 
+    }
 })
 
 module.exports = router; 
