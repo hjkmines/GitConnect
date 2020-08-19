2979f380e1aeb50a5eea2db23af2d00e96d9d48a
Hyung Kim <hjkmines@gmail.com>
2020-08-19 12:44:05 -0600

    adds post route for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,11 @@
 const express = require('express'); 
 const router = express.Router(); 
-const auth = require('../../middleware/auth')
-const User = require('../../models/User')
-const Profile = require('../../models/Profile')
+const auth = require('../../middleware/auth'); 
+const { check, validationResult } = require('express-validator'); 
+
+const User = require('../../models/User'); 
+const Profile = require('../../models/Profile'); 
+
 
 
 //Route: GET api/profile/me 
@@ -23,5 +26,18 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error')
     }
 }); 
+
+//Route: POST api/profile 
+//Details: Create or update user profile  
+//Access: Private 
+router.post('/', [ auth, [
+    check('status', 'Status is required').not().isEmpty(), check('skills', 'Skills is required').not().isEmpty()
+] ],
+async (req, res) => {
+    const errors = validationResult(req); 
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()}); 
+    }
+})
 
 module.exports = router; 
