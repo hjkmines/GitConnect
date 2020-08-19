655099708b5e7f2a942d73823e2f6ff8359431ed
Hyung Kim <hjkmines@gmail.com>
2020-08-19 12:37:57 -0600

    adds route to about me for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require('express'); 
 const router = express.Router(); 
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
+const Profile = require('../../models/Profile')
 
-//Route: GET api/profile 
-//Details: Test route 
-//Access: Public 
-router.get('/', (req, res) => res.send('Hello')); 
+
+//Route: GET api/profile/me 
+//Details: Get current users profiles 
+//Access: Private 
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await (await Profile.findOne({user: req.user.id})).populated('user', 
+        ['name', 'avatar']); 
+
+        if(!profile) {
+            return res.status(400).json({msg: 'There is no profile for this user'}); 
+        }
+
+        res.json(profile); 
+    } catch(err) {
+        console.error(err.message); 
+        res.status(500).send('Server Error')
+    }
+}); 
 
 module.exports = router; 
