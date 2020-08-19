14ef59e854fbcdd1cd004809fa17443d5a9aba06
Hyung Kim <hjkmines@gmail.com>
2020-08-19 17:04:25 -0600

    allows users to create experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -120,7 +120,78 @@ router.get('/user/:user_id', async (req, res) => {
         res.json(profile);  
     } catch (err) {
         console.error(err.message); 
+        if(err.kind === 'ObjectId') {
+            return res.status(400).json({msg: 'Profile not found'})
+        }
+
         res.status(500).send('Server Error'); 
+    }
+})
+
+//Route: DELETE api/profile 
+//Details: Delete profile, user and the posts   
+//Access: Private 
+router.delete('/', auth, async (req, res) => {
+    try {
+        //function: removes users posts 
+
+        //remove user profile 
+        await Profile.findOneAndRemove({user: req.user.id});
+        //Removes the user 
+        await User.findOneAndRemove({_id: req.user.id});
+        res.json({msg: 'User deleted'});  
+    } catch (err) {
+        console.error(err.message); 
+        res.status(500).send('Server Error'); 
+    }
+})
+
+//Route: PUT api/profile/experience 
+//Details: Add profile experience 
+//Access: Private 
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(), 
+    check('company', 'Company is required').not().isEmpty(), 
+    check('from', 'From date is required').not().isEmpty()
+    ]
+], 
+async (req, res) => {
+    const errors = validationResult(req); 
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() }); 
+    } 
+
+    const {
+        title, 
+        company, 
+        location, 
+        from, 
+        to, 
+        current, 
+        description
+    } = req.body; 
+
+    const newExp = {
+        title, 
+        company, 
+        location, 
+        from, 
+        to, 
+        current, 
+        description
+    }
+
+    try {
+     const profile = await Profile.findOne({ user: req.user.id }); 
+
+     profile.experience.unshift(newExp);
+     
+     await profile.save(); 
+
+     res.json(profile); 
+    } catch (err) {
+        console.error(err.message); 
+        res.status(500).send('Server Error')
     }
 })
 
