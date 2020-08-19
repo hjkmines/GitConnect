8347c5108ef5b339d8922e87052ea49e72f2ea2e
Hyung Kim <hjkmines@gmail.com>
2020-08-19 15:53:55 -0600

    adds get all profiles route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,4 +95,17 @@ async (req, res) => {
     }
 })
 
+//Route: GET api/profile 
+//Details: Get all profiles  
+//Access: Public 
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);  
+    } catch (err) {
+        console.error(err.message); 
+        res.status(500).send('Server Error'); 
+    }
+})
+
 module.exports = router; 
