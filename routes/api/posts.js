d89b528a36fa75a248bfe4e64a7c4ffd4f85af77
Hyung Kim <hjkmines@gmail.com>
2020-08-19 19:40:58 -0600

    adds feature to unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -116,10 +116,37 @@ router.put('/like/:id', auth, async (req, res) => {
 
         //check if the post is already liked by same user 
         if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
-            return res.json(400).json({ msg: 'Post already liked' }); 
+            return res.status(400).json({ msg: 'Post already liked' }); 
         }
 
         post.likes.unshift({user: req.user.id}); 
+
+        await post.save(); 
+
+        res.json(post.likes); 
+    } catch (err) {
+        console.error(err.message); 
+        res.status(500).send('Server Error'); 
+    }
+})
+
+
+//Route: PUT api/posts/unlike/:id 
+//Details: Liking a post  
+//Access: Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id); 
+
+        //check if the post is already liked by same user 
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post has not been liked yet' }); 
+        }
+
+        //get remove index 
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id); 
+
+        post.likes.splice(removeIndex, 1); 
 
         await post.save(); 
 
