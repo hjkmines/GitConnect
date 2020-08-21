3a4712f3e28637f1bced4cd26827c7ee454c471d
Hyung Kim <hjkmines@gmail.com>
2020-08-21 10:37:27 -0600

    adds experience to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner'; 
 import ProfileTop from './ProfileTop'; 
 import ProfileAbout from './ProfileAbout'; 
+import ProfileExperience from './ProfileExperience.js'; 
 import { getProfileById, getProfiles } from '../../actions/profile'; 
 
 
@@ -29,6 +30,14 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
             <div class='profile-grid my-1'>
                 <ProfileTop profile={profile} />
                 <ProfileAbout profile={profile} />
+                <div className='profile-exp bg-white p-2'>
+                    <h2 className='text-primary'>Experience</h2>
+                    {profile.experience.length > 0 ? (<Fragment>
+                        {profile.experience.map(experience => (
+                            <ProfileExperience key={experience._id} experience={experience} />
+                        ))}
+                    </Fragment>) : (<h4>No experience credendtials</h4>)}
+                </div>
             </div>
 
             </Fragment>}
