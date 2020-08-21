1e07fd72083b72d1151483a76eff4d94f65867bc
Hyung Kim <hjkmines@gmail.com>
2020-08-21 12:15:20 -0600

    deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const connectDB = require('./config/db')
 const app = express(); 
+const path = require('path'); 
 
 //Connects to the database 
 connectDB(); 
@@ -9,15 +10,21 @@ connectDB();
 //automatically grabs the data from the req.body (client)
 app.use(express.json({extended: false})); 
 
-
-//Tester, will need to remove shortly 
-app.get('/', (req, res) => res.send('Hello!'));
-
 //Define routes for users, auth, profile, and posts
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
+
+//serve static assets in producion 
+if(process.env.NODE_ENV === 'production') {
+    //set static folder 
+    app.use(express.static('client/build')); 
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000; 
 
